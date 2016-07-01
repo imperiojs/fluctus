710d1202727900ad3984f3f813fbd25153e24ddb
Matthew McLaughlin <Matthew McLaughlin>
2016-06-30 20:04:51 -0700

    starting functionality for live acceleration graph

--- client/browser.js
+++ client/browser.js
@@ -48,8 +48,65 @@ function showSocketConnection(room) {
   h6Element.innerHTML = `Socket connection, in ${room}`;
 }
 
+var counter = 0;
+var xDataArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var yDataArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var zDataArray = [0];
 
 // Define socket listeners and callback functions
-frontEndEcho.desktopTapHandler(frontEndEcho.socket, changeBodyClass);
-frontEndEcho.desktopAccelHandler(frontEndEcho.socket, updateAccelerationData);
-frontEndEcho.desktopGyroHandler(frontEndEcho.socket, updateGyroscopeData);
+
+
+var canvas = document.getElementById('accel-chart'),
+    ctx = canvas.getContext('2d'),
+    startingData = {
+      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      datasets: [
+          {
+              fillColor: "rgba(220,220,220,0.2)",
+              strokeColor: "rgba(220,220,220,1)",
+              pointColor: "rgba(220,220,220,1)",
+              pointStrokeColor: "#fff",
+              data: xDataArray
+          },
+          {
+              fillColor: "rgba(151,187,205,0.2)",
+              strokeColor: "rgba(151,187,205,1)",
+              pointColor: "rgba(151,187,205,1)",
+              pointStrokeColor: "#fff",
+              data: yDataArray
+          }
+      ]
+    },
+    latestLabel = startingData.labels[6];
+
+// Reduce the animation steps for demo clarity.
+var myLiveChart = new Chart(ctx).Line(startingData, {
+  animationSteps: 15
+});
+
+
+function updateAccelerationChart(accelerationDataObject) {
+  myLiveChart.addData([accelerationDataObject.alpha, accelerationDataObject.beta], ++latestLabel);
+}
+
+// frontEndEcho.desktopTapHandler(frontEndEcho.socket, changeBodyClass);
+frontEndEcho.desktopAccelHandler(frontEndEcho.socket, printAccel);
+// frontEndEcho.desktopGyroHandler(frontEndEcho.socket, updateGyroscopeData);
+
+function printAccel (accelerationDataObject) {
+  if (counter < 20) {
+    counter ++;
+  } else {
+    counter = 0;
+    myLiveChart.addData([accelerationDataObject.x, accelerationDataObject.y], ++latestLabel);
+    myLiveChart.removeData();
+  }
+}
+
+
+// setInterval(function(){
+//   // Add two random numbers for each dataset
+//   myLiveChart.addData([Math.random() * 100, Math.random() * 100], ++latestLabel);
+//   // Remove the first point so we dont just add values forever
+//   // myLiveChart.removeData();
+// }, 500);
